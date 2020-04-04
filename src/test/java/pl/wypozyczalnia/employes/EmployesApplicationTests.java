e649cd1ea7fd96eabfd8b605274c783cd2d9c73f
patryktk <patrick.tkaczyk@gmail.com>
2020-04-04 20:44:36 +0200

    Initial commit

---
+++ src/test/java/pl/wypozyczalnia/employes/EmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wypozyczalnia.employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
